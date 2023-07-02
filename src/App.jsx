5639d6cc2c4ac3410bf760be4ad9b4847b27cfd8
Monika Rasała <mrasala583@gmail.com>
2023-07-02 17:22:04 +0200

    scss-support

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+  
+  // TU WPISZ HTML
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <section> 
+        <p> Z pewnością widziałaś już nasz szeroki asortyment, w ofercie sklepu zajdziesz kosmetyki przeznaczone do każdego typu cery nawet dla tej najbardziej wymagającej! <br/>
+            Z myślą właśnie o Tobie przygotowaliśmy zakładkę, w której dostaniesz kosmetyki w najniższej cenie na rynku. Chcesz zostać łowcą okazji? — z nami kupisz tanio! <br/>
+            Oferta promocji tygodnia zmienia się nieregularnie, warto bacznie nas obserwować, nie wiadomo kiedy pojawią się koleje perełki. <br/>
+            Uzupełnij swoją kosmetyczkę o dodatkowe kosmetyki do pielęgnacji twarzy i koniecznie zaapatrzyj się w krem z SPF, o którym musisz pamiętać cały rok! <br/>
+            Koniecznie sprawdź, czy w promocji są maseczki pielęgnacyjne, nawilżenie to podstawa. <br/>
+            W twojej kolekcji oczywiście nie może zabraknąć produktów do makijażu, znajdź swój idealny odcień podkładu, posiadamy szeroką gamę kolorów do cery porcelanowej oraz ciemnej i mocno opalonej. <br/>
+            Ściągamy maski, a to dobry powód, aby zaopatrzyć się w nowe produkty do pielęgnacji ust, kolorowa pomadka, a może bezbarwny błyszczyk?  <br/>
+            Jeżeli myślisz o profesjonalnej pielegnacji całego ciała od stóp do głów, koniecznie szukaj w promocyjnej cenie kosmetyków marki Farmona - zapewniają świadome dbanie o włosy i poprawę ich struktury. <br/>
+            Poznawaj nowe marki niedostępne jeszcze w drogeriach i testuj nowości w niskich cenach! Odkrywaj z nami nowości z całego świata! Bądź oryginalna i upoluj trudno dostępne kosmetyki! <br/></p>
+     </section>
+
+    <section>
+        <h1 className="first">Drogeria z kosmetykami eZebra</h1>
+            <storng>Sklep internetowy z kosmetykami i perfumami ezebra.pl </storng>
+                <div className="one">Znajdziesz tu wyjątkowo bogaty asortyment, taki, który uwzględnia potrzeby wszystkich Klientów. Zarówno te cenowe, jak i związane z różnorodnością produktów. Artykuły drogerii internetowej ezebra.pl podzielone zostały na kilka kategorii, co zdecydowanie ułatwia zakupy i pozwala zaoszczędzić czas.<br/>
+                                Odpowiadając na potrzeby Klientów drogeria rozwija swój asortyment i wprowadza do niego kolejne produkty oraz kategorie. Wśród nich znajdują się następujące grupy produktów: Makijaż, Pielęgnacja, Paznokcie, Akcesoria, Perfumy, Zdrowie, Dom, Men, Zestawy Prezentowe, Promocje, Nowości oraz Soczewki.<br/>
+                </div>
+    </section>
+ <section>
+        <h1 className="first">Duży wybór stworzony z myślą o kliencie </h1>
+            <div className="one">
+                Miłośnicy i miłośniczki pięknych zapachów mogą natomiast skorzystać z oferty przygotowanej w zakładce perfumy. Proste w obsłudze i intuicyjne filtry pozwolą dobrać jeden lub kilka flakoników perfum damskich, męskich lub unisex. Bez trudu znajdziesz tutaj to, czego szukasz. Możesz też zestawić dany produkt z innymi.<br/>
+                Jeżeli jeszcze nie wiesz, na co warto się zdecydować, albo wahasz się między jednymi a drugimi kosmetykami, tutaj bez trudu znajdziesz wszelkie potrzebne Ci informacje. A to za sprawą dokładnego i wyczerpującego opisu. Między innymi na temat poszczególnych nut zapachowych.<br/>
+            </div>
+    </section>
+ 
+    <section>
+        <h1 className="first">Sposób na perfekcyjny makijaż </h1>
+            <div className="one">
+                Dzięki gamie produktów do makijażu dostępnych w drogerii eZebra, każdy make-up zostanie wykonany w sposób perfekcyjny. 
+                Miłośniczki takich kosmetyków mogą wybierać wśród: produktów do pielęgnacji ust, pomadek i szminek, błyszczyków, palety cieni do powiek, korektorów, kredek do oczu, podkładów i kremów BB, CC i wielu innych. Co więcej, ceny są tutaj bardzo konkurencyjne, nawet po uwzględnieniu kosztów przesyłki.<br/>
+                Warto na bieżąco sprawdzać aktualne okazje umieszczane w zakładce promocje. Kilku, a nawet kilkudziesięciozłotowe obniżki cen stanowią propozycje, które koniecznie musisz wziąć pod uwagę podczas robienia kolejnych zakupów w sieci. <br/>
+                Dużym ułatwieniem dla każdego, kto odwiedza sklep, jest możliwość wyszukiwania produktów za pomocą wybranych marek. Wśród nich znalazły się m.in.: L'Oreal Paris, Max Factor czy Revlon Professional. <br/>
+                Potrzebujesz kosmetyków? To miejsce jest dla Ciebie! <br/>
+            </div>
+    </section>
+    <section>
+        <h1 className="first">Dla Ciebie i Twojego domu </h1> 
+            <div className="one">
+                Dopełnienie całości stanowi zakładka z akcesoriami. Zestawy do manicure, lusterka kosmetyczne, szczotki do włosów, spinki do włosów i wiele innych tego typu produktów znajdziesz w jednym miejscu. <br/>
+                Akcesoria to również pędzle do makijażu topowych marek. Kupisz je bez potrzeby wyprawy do galerii handlowej. I co ważne, będziesz mieć wystarczającą ilość czasu, aby dokładnie zapoznać się z ich opisem i charakterystyką. <br/>
+                Produkty dostępne w ezebra.pl to jednak nie tylko kosmetyki dla Ciebie, ale również dla Twojego domu. W zakładce Dom znajdziesz m.in. artykuły do aromaterapii, środki czystości czy odświeżacze. <br/>
+                Drogeria ezebra.pl zadba również o Twoją formę psychiczną i fizyczną. W zakładce zdrowie znajdziesz m.in. zdrową żywność oraz suplementy diety. Jeżeli zależy Ci na tym, aby zadbać o swój wygląd, zdrowie i atmosferę w domu eZebra jest dobrym miejscem na zrobienie kompleksowych zakupów. <br/>
+            </div>
+    </section>        
     </>
   )
 }
