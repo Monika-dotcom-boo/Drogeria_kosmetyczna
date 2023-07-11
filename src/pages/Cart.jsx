82361f4cb5aa0eb9275130d5bc2467a4b74d7dea
Monika Rasała <mrasala583@gmail.com>
2023-07-11 23:43:50 +0200

    Outline of cart contents returns

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -7,3 +7,25 @@ function Cart() {
 }
 
 export default Cart;
+
+// const Cart = ({ cartItems }) => {
+//   return (
+//     <div>
+//       <h2>Koszyk</h2>
+//       {cartItems.length === 0 ? (
+//         <p>Koszyk jest pusty.</p>
+//       ) : (
+//         <ul>
+//           {cartItems.map((item) => (
+//             <li key={item.id}>
+//               <span>{item.name}</span>
+//               <span>{item.price}</span>
+//             </li>
+//           ))}
+//         </ul>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Cart;
