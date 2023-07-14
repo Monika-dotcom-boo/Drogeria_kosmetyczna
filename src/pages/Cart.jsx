c2d69336187d02e74083d5f48fe33f7433a93d00
Monika Rasała <mrasala583@gmail.com>
2023-07-14 21:39:38 +0200

    adding favorites to cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,14 +1,14 @@
 import { useProducts } from "../context/productsContext";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faXmark, faArrowUp, faArrowDown } from '@fortawesome/free-solid-svg-icons';
 
 function Cart() {
-  const { cart, addToCart, decreaseQtyInCart, removeFromCart, getProductById} = useProducts()
+  const { cart, addToCart, decreaseQtyInCart, removeFromCart, getProductById, favorites, removeFavorite} = useProducts()
 
   return (
-    <div className="py-10">
-      <h2 className="text-5xl bold mb-10 text-center">Twój koszyk</h2>
-      <table className="w-full">
+    <div className="py-20">
+      <h2 className="text-5xl font-bold mb-10 text-center">Twój koszyk</h2>
+      <table className="w-full mb-20">
         {cart ? cart.map(productInCart => (
           <tr className="border-b border-solid border-gray-400 text-xl">
             <td>
@@ -16,29 +16,50 @@ function Cart() {
             </td>
             <td>{getProductById(productInCart.id).name}</td>
             <td className="text-center">
-              <div>Price:</div>
+              <div className="font-bold">Cena:</div>
               {getProductById(productInCart.id).pricePromo ?? getProductById(productInCart.id).price}
               {' '}
               zł
             </td>
             <td className="text-center">
-              <div>Total:</div>
+              <div className="font-bold">Razem:</div>
               {(getProductById(productInCart.id).pricePromo ?? getProductById(productInCart.id).price) * productInCart.qty}
               {' '}
               zł
             </td>
             <td className="text-center">
-              <div>Quantity:</div>
+              <FontAwesomeIcon className="cursor-pointer" size="xl" onClick={() => addToCart(productInCart.id)} icon={faArrowUp} />
+            </td>
+            <td className="text-center">
+              <div className="font-bold">Ilość:</div>
               {productInCart.qty}
             </td>
-            <td>
-              <button onClick={() => addToCart(productInCart.id)} type="button">Increase</button>
+            <td className="text-center">
+              <FontAwesomeIcon className="cursor-pointer" size="xl" onClick={() => decreaseQtyInCart(productInCart.id)} icon={faArrowDown} />
             </td>
-            <td>
-              <button onClick={() => decreaseQtyInCart(productInCart.id)} type="button">Decrease</button>
-            </td>
-            <td>
+            <td className="text-right pr-5">
               <FontAwesomeIcon className="cursor-pointer" size="xl" onClick={() => removeFromCart(productInCart.id)} icon={faXmark} />
+            </td>
+          </tr>
+      )) : null}
+      </table>
+
+      <h2 className="text-5xl font-bold mb-10 text-center">Twóje ulubione</h2>
+      <table className="w-full">
+        {favorites ? favorites.map(productInFavorites => (
+          <tr className="border-b border-solid border-gray-400 text-xl">
+            <td>
+              <img className="max-w-[50px]" src={getProductById(productInFavorites).image} />
+            </td>
+            <td>{getProductById(productInFavorites).name}</td>
+            <td className="text-center">
+              <div className="font-bold">Cena:</div>
+              {getProductById(productInFavorites).pricePromo ?? getProductById(productInFavorites).price}
+              {' '}
+              zł
+            </td>
+            <td className="text-right pr-5">
+              <FontAwesomeIcon className="cursor-pointer" size="xl" onClick={() => removeFavorite(productInFavorites)} icon={faXmark} />
             </td>
           </tr>
       )) : null}
